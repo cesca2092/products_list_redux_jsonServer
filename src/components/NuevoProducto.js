7f8cc21db2155c21f5281db137d39009d69dc35a
Cesar Caviedes <cesca2092@gmail.com>
2021-10-31 12:20:19 -0500

    feat: alertas

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 //actions de redux
 import { crearNuevoProductoAction } from '../actions/productoActions'
-
+import { mostrarAlerta } from '../actions/alertaActions';
 
 const NuevoProducto = ({history}) => {
 
@@ -17,6 +17,7 @@ const NuevoProducto = ({history}) => {
     //acceder al state del store
     const cargando = useSelector( state => state.productos.loading);
     const error = useSelector( state => state.productos.error);
+    const alerta = useSelector( state => state.alerta.alerta);
 
     //llamar el action de productoAction
     const agregarProducto = producto => dispatch(crearNuevoProductoAction(producto))
@@ -27,6 +28,13 @@ const NuevoProducto = ({history}) => {
 
         //validar formulario
         if(nombre.trim() === '' || precio <= 0){
+
+            const alerta ={
+                msg: 'Ambos campos son obligatorios,',
+                classes: 'alert alert-danger text-center text-uppercase p3'
+            }
+
+            dispatch( mostrarAlerta(alerta) )
             return;
         }
 
@@ -51,6 +59,8 @@ const NuevoProducto = ({history}) => {
                         <h2 className='text-center mb-4 font-weight-bold'>
                             Agregar Nuevo Producto
                         </h2>
+
+                        {alerta ? <p className={alerta.classes}>{alerta.msg} </p> : null}
 
                         <form
                             onSubmit={submitNuevoProducto}
